32dee814f326672da8e63239c2e6b1a592c6edbd
jakenielson <groakayt@gmail.com>
2017-10-17 16:10:11 -0700

    Change post preview to media object

--- frontend/src/components/PostPreview.js
+++ frontend/src/components/PostPreview.js
@@ -2,16 +2,14 @@ import React from 'react';
 
 export default function PostPreview ({ post }) {
   return (
-    <div className="post-preview container">
-      <div className="row">
-        <div className="col-2">
-          <i className="fa fa-arrow-up"></i>
-          <p>{ post.voteScore }</p>
-          <i className="fa fa-arrow-down"></i>
-        </div>
-        <div className="col-10">
-          <h3>{post.title}</h3>
-        </div>
+    <div className="post-preview media m-2">
+      <div className="d-flex flex-column m-3">
+        <i className="fa fa-arrow-up"></i>
+        <span className="text-center">{ post.voteScore }</span>
+        <i className="fa fa-arrow-down"></i>
+      </div>
+      <div className="media-body m-3">
+        <h3>{ post.title }</h3>
       </div>
     </div>
   );
