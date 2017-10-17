ae629c6cf0a007e9b4d485ac9092fa66396a3529
jakenielson <groakayt@gmail.com>
2017-10-17 14:00:32 -0700

    Update navbar with react Links

--- frontend/src/components/PageHeader.js
+++ frontend/src/components/PageHeader.js
@@ -3,31 +3,19 @@ import { Link } from 'react-router-dom'
 
 export default function PageHeader ({ name }) {
   return (
-    <nav className="navbar navbar-expand-sm navbar-light bg-light fixed-top">
+    <nav className="navbar navbar-expand-sm navbar-light bg-light">
       <div className="container">
         <button className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <a href="index.html" className="navbar-brand">Jake Nielson</a>
+        <Link to="/" className="navbar-brand">{ name }</Link>
         <div className="collapse navbar-collapse" id="navbarCollapse">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
-              <a href="#about-section" className="nav-link">About</a>
+              <Link to="/react" className="nav-link">React</Link>
             </li>
             <li className="nav-item">
-              <a href="#skills-section-head" className="nav-link">Skills</a>
-            </li>
-            <li className="nav-item">
-              <a href="#projects-section-head" className="nav-link">Projects</a>
-            </li>
-            <li className="nav-item">
-              <a href="#education-section-head" className="nav-link">Education</a>
-            </li>
-            <li className="nav-item">
-              <a href="#experience-section-head" className="nav-link">Experience</a>
-            </li>
-            <li className="nav-item">
-              <a href="#contact-footer-head" className="nav-link">Contact</a>
+              <Link to="/redux" className="nav-link">Redux</Link>
             </li>
           </ul>
         </div>
